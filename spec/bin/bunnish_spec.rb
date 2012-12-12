593bbd903e19160b6e06893fc20c702d58041893
Kenji Hara <haracane@gmail.com>
2012-12-12 10:56:39 +0900

    Version bump to 0.0.8

--- spec/bin/bunnish_spec.rb
+++ spec/bin/bunnish_spec.rb
@@ -21,14 +21,14 @@ describe "bin/bunnish" do
   end
   describe "count" do
     it "should output valid queue count" do
-      result = `#{Bunnish::RUBY_CMD} #{Bunnish::BIN_DIR}/bunnish count bunnish-test-queue #{@stderr_dst}`
+      result = `#{Bunnish::RUBY_CMD} #{Bunnish::BIN_DIR}/bunnish count bunnish-test-queue --verbose #{@stderr_dst}`
       result.chomp!
       result.should == "3"
     end
   end
   describe "delete" do
     it "should delete queue" do
-      `#{Bunnish::RUBY_CMD} #{Bunnish::BIN_DIR}/bunnish delete bunnish-test-queue #{@stderr_dst}`
+      `#{Bunnish::RUBY_CMD} #{Bunnish::BIN_DIR}/bunnish delete bunnish-test-queue --verbose #{@stderr_dst}`
       result = `#{Bunnish::RUBY_CMD} #{Bunnish::BIN_DIR}/bunnish count bunnish-test-queue 2> /dev/null`
       result = result.chomp!
       result.should == "0"
@@ -36,7 +36,7 @@ describe "bin/bunnish" do
   end
   describe "help" do
     it "should delete queue" do
-      result = `#{Bunnish::RUBY_CMD} #{Bunnish::BIN_DIR}/bunnish help bunnish-test-queue #{@stderr_dst}`
+      result = `#{Bunnish::RUBY_CMD} #{Bunnish::BIN_DIR}/bunnish help bunnish-test-queue --verbose #{@stderr_dst}`
       result.split().size.should > 0
       $?.should == 0
     end
@@ -44,7 +44,7 @@ describe "bin/bunnish" do
   describe "publish" do
     context "when exchange-name is not set" do
       it "should publish valid messages with direct exchange" do
-        `cat #{@input_file.path} | #{Bunnish::RUBY_CMD} #{Bunnish::BIN_DIR}/bunnish publish bunnish-test-queue,bunnish-test-queue-2 #{@stderr_dst}`
+        `cat #{@input_file.path} | #{Bunnish::RUBY_CMD} #{Bunnish::BIN_DIR}/bunnish publish bunnish-test-queue,bunnish-test-queue-2 --verbose #{@stderr_dst}`
         result = `#{Bunnish::RUBY_CMD} #{Bunnish::BIN_DIR}/bunnish subscribe bunnish-test-queue 2> /dev/null`
         result = result.split()
         # result.each do |line| STDERR.puts line end
@@ -67,7 +67,7 @@ describe "bin/bunnish" do
     
     context "when exchange-name is set" do
       it "should publish valid messages with fanout exchange" do
-        `cat #{@input_file.path} | #{Bunnish::RUBY_CMD} #{Bunnish::BIN_DIR}/bunnish publish --exchange-name bunnish-test-exchange bunnish-test-queue,bunnish-test-queue-2 #{@stderr_dst}`
+        `cat #{@input_file.path} | #{Bunnish::RUBY_CMD} #{Bunnish::BIN_DIR}/bunnish publish --exchange-name bunnish-test-exchange bunnish-test-queue,bunnish-test-queue-2 --verbose #{@stderr_dst}`
         result = `#{Bunnish::RUBY_CMD} #{Bunnish::BIN_DIR}/bunnish subscribe bunnish-test-queue 2> /dev/null`
         result = result.split()
         # result.each do |line| STDERR.puts line end
@@ -90,7 +90,7 @@ describe "bin/bunnish" do
   end
   describe "status" do
     it "should print valid status of message queue" do
-      result = `#{Bunnish::RUBY_CMD} #{Bunnish::BIN_DIR}/bunnish status bunnish-test-queue #{@stderr_dst}`
+      result = `#{Bunnish::RUBY_CMD} #{Bunnish::BIN_DIR}/bunnish status bunnish-test-queue --verbose #{@stderr_dst}`
       result = result.split(/\n/)
       # STDERR.puts result.inspect
       result.include?("bunnish-test-queue : 3 messages, 0 consumers").should be_true
@@ -98,7 +98,7 @@ describe "bin/bunnish" do
   end
   describe "subscribe" do
     it "should subscribe valid messages" do
-      result = `#{Bunnish::RUBY_CMD} #{Bunnish::BIN_DIR}/bunnish subscribe bunnish-test-queue #{@stderr_dst}`
+      result = `#{Bunnish::RUBY_CMD} #{Bunnish::BIN_DIR}/bunnish subscribe bunnish-test-queue --verbose #{@stderr_dst}`
       result = result.split()
       # result.each do |line| STDERR.puts line end
       result.shift.should == "foo"
