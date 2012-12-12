bf0d73fec417e79071dae9a0dd99f8ae0a81f61d
Kenji Hara <haracane@gmail.com>
2012-12-12 16:51:42 +0900

    correct rspec description

--- spec/lib/bunnish/command/help_spec.rb
+++ spec/lib/bunnish/command/help_spec.rb
@@ -1,7 +1,7 @@
 require "spec_helper"
 
 describe Bunnish::Command::Help do
-  describe "#run(argv, input_stream, output_stream, error_stream)" do
+  describe ".run(argv, input_stream, output_stream, error_stream)" do
     context "when argv = []" do
       it "should success" do
         
